3c778820e62d694d8be7ca9805c6e119b16543a4
Padmanabha Roy <73742277+roypadmanabha@users.noreply.github.com>
2023-09-17 15:54:12 +0530

    Update dot.js

--- dot.js
+++ dot.js
@@ -15,18 +15,4 @@ document.addEventListener("keydown", function (event) {
         event.preventDefault();
     }
 });
-<script>
-        // Prevent zooming via pinch gesture on touch devices
-        document.addEventListener('touchmove', function(event) {
-            if (event.touches.length > 1) {
-                event.preventDefault();
-            }
-        }, { passive: false });
 
-        // Prevent zooming via Ctrl + Mousewheel on desktop browsers
-        window.addEventListener('wheel', function(event) {
-            if (event.ctrlKey) {
-                event.preventDefault();
-            }
-        });
-    </script>
